b456545d33921b90e69d9c51962510be2b8107d6
Jennifer Zhang <jxzhang@post.harvard.edu>
2013-11-21 20:10:51 -0800

    Basic functionality to read/write a file less than PSIZE bytes

--- Receiver/receiver.c
+++ Receiver/receiver.c
@@ -6,12 +6,21 @@
 #include <netdb.h>
 #include <netinet/in.h>
 
+#define PSIZE 1000
+
+struct header {
+    int sequence;
+};
+
 
 // argv: sender hostname, sender portnumber, filename, Pl, PC
 int main (int argc, char *argv[]) {
-    int sockfd, portno;
+    int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
+    FILE *fp;
+    
+    char buffer[PSIZE];
     
     portno = atoi(argv[2]);
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
@@ -30,8 +39,20 @@ int main (int argc, char *argv[]) {
     memcpy ((void *)&serv_addr.sin_addr, server->h_addr_list[0], server->h_length);
     serv_addr.sin_port = htons(portno);
     
-    if (sendto (sockfd, "asdf", 4, 0, (struct sockaddr *)&serv_addr, sizeof (serv_addr)) < 0) {
+    n = strlen(argv[3]);
+    if (sendto (sockfd, argv[3], n, 0, (struct sockaddr *)&serv_addr, sizeof (serv_addr)) < 0) {
         perror ("Sendto failed");
         return 0;
     }
+    
+    fp = fopen(argv[3], "w");
+    
+    while(1) {
+        n = recvfrom(sockfd, buffer, PSIZE, 0, NULL, 0);
+        buffer[n] = 0;
+        fwrite(buffer, 1, n, fp);
+        break;
+    }
+    
+    fclose(fp);
 }
--- Sender/sender.c
+++ Sender/sender.c
@@ -6,34 +6,53 @@
 #include <netdb.h>
 #include <netinet/in.h>
 
+#define PSIZE 1000
+
 // argv: portnumber, Cwnd, Pl, PC
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
     int sockfd, portno, n;
+    size_t f;
     struct sockaddr_in serv_addr, cli_addr;
     socklen_t addrlen;
+    FILE *fp;
     
-    char buffer[1000];
+    char buffer[PSIZE];
     
     portno = atoi(argv[1]);
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror ("Error creating socket");
         return 0;
     }
     
-    memset ((char *)&serv_addr, 0, sizeof (serv_addr));
+    memset((char *)&serv_addr, 0, sizeof (serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
     
-    if (bind (sockfd, (struct sockaddr *)&serv_addr, sizeof (serv_addr)) < 0) {
-        perror ("Error on bind");
+    if(bind(sockfd, (struct sockaddr *)&serv_addr, sizeof (serv_addr)) < 0) {
+        perror("Error on bind");
         return 0;
     };
     
-    while (1) {
+    while(1) {
         addrlen = sizeof(cli_addr);
-        n = recvfrom (sockfd, buffer, 1000, 0, (struct sockaddr *)&cli_addr, &addrlen);
+        n = recvfrom(sockfd, buffer, PSIZE, 0, (struct sockaddr *)&cli_addr, &addrlen);
         buffer[n] = 0;
         printf("Received: %s\n", buffer);
+        
+        fp = fopen (buffer, "r");
+        if (fp == 0)
+            printf("Requested file %s does not exist\n", buffer);
+        
+        f = fread(buffer, 1, PSIZE, fp);
+        
+        if (sendto(sockfd, buffer, f, 0, (struct sockaddr *)&cli_addr, addrlen) < 0) {
+            perror("Sendto failed");
+            return 0;
+        }
+        
+        break;
     }
+    
+    fclose(fp);
 }
